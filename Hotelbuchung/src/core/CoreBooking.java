b972cfb66fbd65590fad64e9f7c8402744b91ddf
wickds <stefan_kasprzak@yahoo.de>
2015-04-29 10:45:05 +0200

    Buchungsprüfung added

--- Hotelbuchung/src/core/CoreBooking.java
+++ Hotelbuchung/src/core/CoreBooking.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
@@ -25,9 +26,10 @@ public class CoreBooking implements ActionListener {
 	public static String dateuntilyy;
 	public static String isAvailable;
 	private GuiMain parent;
+	private String customers = "";
 	
 	public CoreBooking(GuiMain parent) {
-		super();
+			super();
 		this.parent = parent;
 	}
 
@@ -42,49 +44,55 @@ public class CoreBooking implements ActionListener {
 		String actionCommand = e.getActionCommand();
 		
 		if (actionCommand.equals("Buchen")){
-			int reply = JOptionPane.showConfirmDialog(null, "Jetzt verbindlich Buchen?", "Buchen", JOptionPane.YES_NO_OPTION);
-					
-			if (reply == JOptionPane.YES_OPTION) {
-				
-				customer = (String) parent.getComboBox().getSelectedItem();
-				category = (String) parent.getJComboBox_1().getSelectedItem();
-				arrangement = (String) parent.getJComboBox_2().getSelectedItem();
-				datefromdd = (String) parent.getJComboBox_3().getSelectedItem();
-				datefrommm = (String) parent.getJComboBox_4().getSelectedItem();
-				datefromyy = (String) parent.getJComboBox_5().getSelectedItem();
-				dateuntildd = (String) parent.getJComboBox_7().getSelectedItem();
-				dateuntilmm = (String) parent.getJComboBox_8().getSelectedItem();
-				dateuntilyy = (String) parent.getJComboBox_6().getSelectedItem();
-				try {
-					try {
-						ReadWrite.WriteBooking();
-						ReadWrite.ReadFromFile();
+			
+			customers = (String) parent.getComboBox().getSelectedItem();
+			
+			if(GuiMain.comboboxModelBookings.getIndexOf(customers) == -1){		
+					int reply = JOptionPane.showConfirmDialog(null, "Jetzt verbindlich Buchen?", "Buchen", JOptionPane.YES_NO_OPTION);
+			
+					if (reply == JOptionPane.YES_OPTION) {
 						
-						if(category == "Economy"){
-							ReadWrite.economyCount = ReadWrite.economyCount +1;
-						} else if(category == "Business"){
-							ReadWrite.businessCount = ReadWrite.businessCount +1;
-						} else if(category == "Suite"){
-							ReadWrite.suiteCount = ReadWrite.suiteCount +1;
+						customer = (String) parent.getComboBox().getSelectedItem();
+						category = (String) parent.getJComboBox_1().getSelectedItem();
+						arrangement = (String) parent.getJComboBox_2().getSelectedItem();
+						datefromdd = (String) parent.getJComboBox_3().getSelectedItem();
+						datefrommm = (String) parent.getJComboBox_4().getSelectedItem();
+						datefromyy = (String) parent.getJComboBox_5().getSelectedItem();
+						dateuntildd = (String) parent.getJComboBox_7().getSelectedItem();
+						dateuntilmm = (String) parent.getJComboBox_8().getSelectedItem();
+						dateuntilyy = (String) parent.getJComboBox_6().getSelectedItem();
+						try {
+							try {
+								ReadWrite.WriteBooking();
+								ReadWrite.ReadFromFile();
+								
+								if(category == "Economy"){
+									ReadWrite.economyCount = ReadWrite.economyCount +1;
+								} else if(category == "Business"){
+									ReadWrite.businessCount = ReadWrite.businessCount +1;
+								} else if(category == "Suite"){
+									ReadWrite.suiteCount = ReadWrite.suiteCount +1;
+								}
+								ReadWrite.WriteStatistics();
+								ReadWrite.LoadStatistics();
+							} catch (JDOMException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							GuiMain.comboboxModelBookings.removeAllElements();
+							ReadWrite.cancelBookingList.toArray(); // to array :)
+							for (String customer : ReadWrite.cancelBookingList){
+								GuiMain.comboboxModelBookings.addElement(customer);
+							}
+							parent.getCancelationCustomer().setModel(GuiMain.comboboxModelBookings);
+							
+						} catch (IOException ioe) {
+							ioe.printStackTrace();
 						}
-						ReadWrite.WriteStatistics();
-						ReadWrite.LoadStatistics();
-					} catch (JDOMException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
 					}
-					GuiMain.comboboxModelBookings.removeAllElements();
-					ReadWrite.cancelBookingList.toArray(); // to array :)
-					for (String customer : ReadWrite.cancelBookingList){
-						GuiMain.comboboxModelBookings.addElement(customer);
+				}else{
+					JOptionPane.showMessageDialog(null, "Es ist schon eine Buchung für diesen Kunden vorhanden!\n Bitte löschen Sie diese zuerst!");
 					}
-					parent.getCancelationCustomer().setModel(GuiMain.comboboxModelBookings);
-					
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
-				}
-			} 
-		}
 		if (actionCommand.equals("Kunden löschen")){
 			
 			int reply1 = JOptionPane.showConfirmDialog(null, "Kunden wirklich löschen?", "löschen", JOptionPane.YES_NO_OPTION);
@@ -126,4 +134,5 @@ public class CoreBooking implements ActionListener {
 			
 		
 		}
+	}
 }
